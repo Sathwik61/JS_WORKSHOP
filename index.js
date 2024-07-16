d6f7be20f9cc51168c3d089fc43ea485209524c6
SATHWIK <sathwikacharya61@gmail.com>
2024-07-16 11:58:05 +0530

    hshs

--- index.js
+++ index.js
@@ -37,17 +37,55 @@
 // }
 
 // let a= 'abc';
-console.log(`hey ${a}`)
-function func(){
-    console.log("hey there!")
+// console.log(`hey ${a}`)
+// function func(){
+//     console.log("hey there!")
+// }
+// func();
+
+// document.addEventListener("click",()=>{
+//     console.log("djbf")
+// })
+
+// const fun3=()=>{
+
+// }
+// fun3()
+
+// const btn=document.getElementById('btn')
+// btn.addEventListener("click",()=>{
+//     alert("Clicked ")
+// })
+
+
+
+
+// function delay(item){
+//     return item*5;
+// }
+
+// const result = delay(300)
+
+// console.log(result)
+// let a=9
+// for(let i=0;i<5;i++){
+//     console.log(i)
+// }
+
+// let arr=[1,2,3,4,5]
+
+// for( let i in arr)
+// {
+//     console.log(i)
+// }
+
+
+const obj ={
+    name:"ABC",
+    age:34,
+    section:'d'
 }
-func();
 
-document.addEventListener("click",()=>{
-    console.log("djbf")
-})
-
-const fun3=()=>{
-
+for( let i in obj){
+    console.log(`key :${i} \t value:${obj[i]}`) 
 }
-fun3()
