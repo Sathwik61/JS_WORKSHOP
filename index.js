18f307f2caf239ca5d94edd0e921b898162a4167
SATHWIK <sathwikacharya61@gmail.com>
2024-07-16 10:06:01 +0530

    array

--- index.js
+++ index.js
@@ -3,10 +3,20 @@
 //  a=99;
 // console.log(a)
 
-let a=10;
-let b=12.23
-let c=true
+// let a=10;
+// let b=12.23
+// let c=true
 
-console.log(a)
-console.log(b)
-console.log(typeof(c))
+// console.log(a)
+// console.log(b)
+// console.log(typeof(c))
+
+// const obj ={
+//     name:"abc",
+//     age:15
+// }
+// console.log(obj);
+
+
+let arr =[1,2,3,4,5,6,7]
+console.log(arr)
